ec66a4f9b3740176dfff91f3e9688291b36d1eee
Anwesha Das <anwesharimi123@gmail.com>
2024-12-27 02:19:46 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -5,23 +5,33 @@ import "dotenv/config";
 import connectDB from "./config/db.js";
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import companyRoutes from "./routes/companyRoutes.js";
+import connectCloudinary from "./config/cloudinary.js";
+import jobRoutes from "./routes/jobRoutes.js";
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express';
 
 // Initialize express
 const app = express();
 
 // Connect to Database
 await connectDB();
+await connectCloudinary();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 //Routes
 app.get("/", (req, res) => res.send("API Working"));
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
-app.post('/webhooks',clerkWebhooks)
+app.post("/webhooks", clerkWebhooks);
+app.use("/api/company", companyRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use('/api/users', userRoutes)
 
 // Port
 const PORT = process.env.PORT || 5000;
